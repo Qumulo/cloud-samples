15e2e04d4383598d7766550f2629538acd80fcc8
JohnMcGJr <40901624+JohnMcGJr@users.noreply.github.com>
2018-07-06 14:12:49 -0700

    Securing the security group
    Removed fully open security group, in favor of a security group that only opens the ports needed for full functionality.

--- Terraform/qumulo.tf
+++ Terraform/qumulo.tf
@@ -17,43 +17,54 @@
  */
 
 // Specify the region in which you want to run QF2.
-variable aws_region { type = "string" }
+variable aws_region {
+  type = "string"
+}
 
 // Specify the subnet in which you want to run QF2.
-variable aws_subnet_id { type = "string" }
+variable aws_subnet_id {
+  type = "string"
+}
 
 // Specify the admin password you want to set for you QF2 cluster.
-variable cluster_admin_password { type = "string" }
+variable cluster_admin_password {
+  type = "string"
+}
 
 // Specify the name of the SSH key pair that you want to launch your EC2
 // instances with. This has to be the name of a key pair that you've already
 // uploaded to AWS.
-variable ssh_key_name { type = "string" }
+variable ssh_key_name {
+  type = "string"
+}
 
 // Specify the local path to the private key or .pem for the key pair that
 // you've chosen above.
-variable ssh_key_path { type = "string" }
+variable ssh_key_path {
+  type = "string"
+}
 
 // Optionally, specify the AMI id for the version of QF2 that you want to
 // launch.
 variable ami_id {
-  type = "string"
+  type    = "string"
   default = ""
 }
 
 // Optionally, set a username that will be used to tag the provisioned EC2
 // instances.
 variable username {
-  type = "string"
+  type    = "string"
   default = ""
 }
 
 // Optionally, set the cluster name, size, and underlying EC2 instance type.
 variable "cluster_config" {
   type = "map"
+
   default = {
-    cluster_name = "Qumulo"
-    node_count = 4
+    cluster_name  = "Qumulo"
+    node_count    = 4
     instance_type = "m4.16xlarge"
   }
 }
@@ -73,19 +84,100 @@ data "aws_subnet" "selected" {
 resource "aws_security_group" "allow_all" {
   name_prefix = "se_demo"
   description = "Allows all traffic for a sales demo"
-  vpc_id = "${data.aws_subnet.selected.vpc_id}"
+  vpc_id      = "${data.aws_subnet.selected.vpc_id}"
 
   ingress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP-to-HTTPS redirect"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTPS"
+  }
+
+  ingress {
+    from_port   = 445
+    to_port     = 445
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SMB"
+  }
+
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "NFS"
+  }
+
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "NFS"
+  }
+
+  ingress {
+    from_port   = 111
+    to_port     = 111
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "NFS port mapper"
+  }
+
+  ingress {
+    from_port   = 111
+    to_port     = 111
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "NFS port mapper"
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "GUI"
+  }
+
+  ingress {
+    from_port   = 3712
+    to_port     = 3712
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "QF2 Replication"
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true
+    description = "Qumulo Intranode Communication"
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -93,8 +185,8 @@ resource "aws_security_group" "allow_all" {
 // Find the latest AMI owned by Qumulo's engineering AWS account.
 data "aws_ami" "latest" {
   most_recent = true
-  owners = [ "343459513285" ] // This is the Qumulo Engineering account id
-  name_regex = "^Qumulo-Cloud.*-release-.*$"
+  owners      = ["343459513285"]              // This is the Qumulo Engineering account id
+  name_regex  = "^Qumulo-Cloud.*-release-.*$"
 }
 
 // If you specified a specific AMI using `-var ami_id=$AMI_ID` when running this
@@ -105,12 +197,13 @@ locals {
 
 // Create the nodes and ensure that QF2 comes up on each.
 resource "aws_instance" "node" {
-  count = "${var.cluster_config["node_count"]}"
-  ami = "${local.ami_id}"
-  instance_type = "${var.cluster_config["instance_type"]}"
-  key_name = "${var.ssh_key_name}"
-  subnet_id = "${data.aws_subnet.selected.id}"
-  vpc_security_group_ids = [ "${aws_security_group.allow_all.id}" ]
+  count                  = "${var.cluster_config["node_count"]}"
+  ami                    = "${local.ami_id}"
+  instance_type          = "${var.cluster_config["instance_type"]}"
+  key_name               = "${var.ssh_key_name}"
+  subnet_id              = "${data.aws_subnet.selected.id}"
+  vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+
   tags {
     Name = "${var.cluster_config["cluster_name"]} ${count.index + 1}"
     User = "${var.username}"
@@ -118,10 +211,11 @@ resource "aws_instance" "node" {
 
   // Don't consider a node as up until qq is up
   provisioner "remote-exec" {
-    inline = [ "until qq version; do sleep 1; done;" ]
+    inline = ["until qq version; do sleep 1; done;"]
+
     connection = {
-      type = "ssh"
-      user = "admin"
+      type        = "ssh"
+      user        = "admin"
       private_key = "${file("${var.ssh_key_path}")}"
     }
   }
@@ -130,8 +224,8 @@ resource "aws_instance" "node" {
 // This block is used to decide if we have to SSH into a public or private IP
 // when clustering our nodes.
 locals {
-  public_ip = "${aws_instance.node.0.public_ip}"
-  private_ip ="${aws_instance.node.0.private_ip}"
+  public_ip    = "${aws_instance.node.0.public_ip}"
+  private_ip   = "${aws_instance.node.0.private_ip}"
   lead_node_ip = "${ local.public_ip != "" ? local.public_ip : local.private_ip }"
 }
 
@@ -142,7 +236,7 @@ resource "null_resource" "cluster_nodes" {
   }
 
   provisioner "remote-exec" {
-    inline = [ <<SCRIPT
+    inline = [<<SCRIPT
 # Cluster the nodes
 qq cluster_create --accept-eula \
   --cluster-name ${var.cluster_config["cluster_name"]} \
@@ -155,9 +249,9 @@ SCRIPT
     ]
 
     connection {
-      type = "ssh"
-      host = "${local.lead_node_ip}"
-      user = "admin"
+      type        = "ssh"
+      host        = "${local.lead_node_ip}"
+      user        = "admin"
       private_key = "${file("${var.ssh_key_path}")}"
     }
   }
@@ -166,6 +260,7 @@ SCRIPT
 output "Private IP Addresses" {
   value = ["${aws_instance.node.*.private_ip}"]
 }
+
 output "Public IP Addresses" {
   value = ["${aws_instance.node.*.public_ip}"]
 }
